e68a7ddca15a25f562ecd55abaf246708ebff369
qixm <qixm@burnish.cn>
2022-07-06 13:40:42 +0800

    feat: add devServer config

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ const webpack = require("webpack")
 const path = require("path")
 const pkg = require("./package.json")
 
+const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 const __APP_INFO__ = {
   pkg: {
     name: pkg.name,
@@ -23,6 +24,24 @@ const name = "Vue2.7 Admin"
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: process.env.BUILD_MODE !== "prod",
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://www.ykt.com/',//接口域名
+    //     changeOrigin: true, //改变源：host地址
+    //     secure: false, // 接受 运行在 https 上的服务
+    //     pathRewrite: {
+    //       '^/api': '/index/index'//需要rewrite重写
+    //     }
+    //   }
+    // }
+  },
   configureWebpack: {
     name: name,
     resolve: {
